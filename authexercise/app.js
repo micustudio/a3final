60a932906883b42f251940266c3be457d5a5e4ae
micustudio <mmmmmaguro@gmail.com>
2015-11-19 17:51:38 +0000

    another commit

--- authexercise/app.js
+++ authexercise/app.js
@@ -23,6 +23,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Configuring Passport
+var passport = require('passport');
+var expressSession = require('express-session');
+// TODO - Why Do we need this key ?
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', routes);
 app.use('/users', users);
 
